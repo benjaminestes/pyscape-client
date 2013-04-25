0091455614546b8bc737712193903e1713986067
Benjamin Estes <benjamin.estes@distilled.net>
2013-04-24 21:47:08 -0700

    clean up some testing lines

--- pyscape.py
+++ pyscape.py
@@ -15,8 +15,6 @@ class Pyscape:
         auth_string = access_id + ':' + secret_key
         base64string = base64.b64encode(auth_string.encode('utf-8'))
         self.auth = base64string.decode('utf-8') 
-        print(auth_string)
-        print(self.auth)
     
     def call(self, method, url, params = None, tries = 5):
         # If no parameters specified, choose intelligent default
@@ -28,32 +26,26 @@ class Pyscape:
         
         full_query = self.baseurl + method + '/' + \
                      url + '?' + query_string
-        print(full_query)
 
         request = urllib.request.Request(full_query)
         request.add_header("Authorization", "Basic %s" % self.auth)
 
-        json_data = []
-
-        #for i in range(1, tries):
-        # print('Fetching data for:', url, end='... ')
         raw = urllib.request.urlopen(request)
         json_data = json.loads(raw.read().decode('utf-8'))
 
         return json_data
 
 def main():
-    command = sys.argv[1] if len(sys.argv) > 1 else None
-    resource = sys.argv[1] if len(sys.argv) > 1 else None
-    csvfile = sys.argv[1] if len(sys.argv) > 1 else None
+    url = sys.argv[1] if len(sys.argv) > 1 else None
+
+    if not url:
+        exit('Invalid URL.')
 
     with open('keys.json', 'r') as k:
         key_string = json.load(k)
 
-    print(key_string['access-id'], key_string['secret-key'])
-    
     pys = Pyscape(key_string['access-id'], key_string['secret-key'])
-    print(pys.call('url-metrics', 'http://www.distilled.net'))
+    print(pys.call(Pyscape.U, url))
 
 if __name__ == '__main__':
     sys.exit(main())
