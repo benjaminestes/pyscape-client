551b289e0e5c0fac74a59138efe784c249f0a2fb
Benjamin Estes <benjamin.estes@distilled.net>
2013-04-25 20:12:42 -0700

    scope selection mechanism for links and anchor-text calls

--- pyscape.py
+++ pyscape.py
@@ -8,9 +8,26 @@ import base64
 
 class Pyscape:
     "Facilitate grabbing data from Mozscape API."
+    # API call names
     A = 'anchor-text'
     L = 'links'
     U = 'url-metrics'
+
+    # Links API scope names
+    L_PTP = 'page_to_page'
+    L_PTS = 'page_to_subdomain'
+    L_PTD = 'page_to_domain'
+    L_DTP = 'domain_to_page'
+    L_DTP = 'domain_to_subdomain'
+    L_DTP = 'domain_to_domain'
+
+    # Anchor text API scope names
+    A_PTP = 'phrase_to_page'
+    A_PTS = 'phrase_to_subdomain'
+    A_PTD = 'phrase_to_domain'
+    A_TTP = 'term_to_page'
+    A_TTP = 'term_to_subdomain'
+    A_TTP = 'term_to_domain'
 
     def __init__(self, access_id, secret_key):
         self.baseurl = 'http://lsapi.seomoz.com/linkscape/' 
@@ -19,6 +36,8 @@ class Pyscape:
         self.auth = base64string.decode('utf-8') 
     
     def call(self, method, url, params = None, tries = 5):
+        json_data = None
+
         query_string = '&'.join([k + '=' + urllib.parse.quote(str(v)) \
                                  for (k, v) in params.items()])
         
@@ -28,34 +47,48 @@ class Pyscape:
         request = urllib.request.Request(full_query)
         request.add_header("Authorization", "Basic %s" % self.auth)
 
-        raw = urllib.request.urlopen(request)
-        json_data = json.loads(raw.read().decode('utf-8'))
+        try:
+            raw = urllib.request.urlopen(request)
+            json_data = json.loads(raw.read().decode('utf-8'))
+        except:
+            # Return json_data with a value None
+            pass
 
         return json_data
 
-    def anchor_text(self, url):
-        a_params = {'Scope': 'phrase_to_page',
-                         'Sort': 'domains_linking_page',
-                         'Cols': 2}
+    def anchor_text(self, url, scope = A_PTP):
+        a_params = {'Scope': scope,
+                    'Sort': 'domains_linking_page',
+                    'Cols': 2}
         
         return self.call(Pyscape.A, url, a_params)
 
-    def links(self, url):
-        l_params = {'SourceCols': 4,
-                         'TargetCols': 4,
-                         'LinkCols': 2,
-                         'Scope': 'page_to_domain',
-                         'Sort': 'page_authority',
-                         'Limit': 5,
-                         'Offset': 0}
-        
+    def links(self, url, scope = L_PTP):
         data = []
-        offset = 0
-        step = 5
 
-        for i in range(3):
+        # API documentation says 50 links per request
+        # is optimal
+        offset = 0
+        step = 50
+
+        l_params = {'SourceCols': 4,
+                    'TargetCols': 4,
+                    'LinkCols': 2,
+                    'Scope': scope,
+                    'Sort': 'page_authority',
+                    'Limit': step,
+                    'Offset': offset}
+
+        # Limited to 100,000 links total
+        for i in range(int(100000 / step)):
             l_params['Offset'] = offset
-            data.extend(self.call(Pyscape.L, url, l_params))
+            call_data = self.call(Pyscape.L, url, l_params)
+            if call_data:
+                # If data was received...
+                data.extend(call_data)
+            else:
+                # If call() failed to return data, stop collecting
+                break   
             offset = offset + step
         
         return data
@@ -77,13 +110,14 @@ def help():
 
 def main():
     method = sys.argv[1] if len(sys.argv) > 1 else None
-    resource = sys.argv[2] if len(sys.argv) > 2 else None
+    option1 = sys.argv[2] if len(sys.argv) > 2 else None
+    option2 = sys.argv[3] if len(sys.argv) > 3 else None
 
     if not method:
         help()
         sys.exit()
 
-    if not resource:
+    if not option1:
         help()
         exit('Invalid URL.')
 
@@ -93,20 +127,38 @@ def main():
     pys = Pyscape(key_string['access-id'], key_string['secret-key'])
     
     if method == 'url-metrics':
-        url = resource
+        url = option1
         print(pys.url_metrics(url))
     elif method == 'bulk-metrics':
         urls = []
-        with open(resource, 'r') as j:
+        with open(option1, 'r') as j:
             for line in j:
                 urls.append(line.rstrip())
         print(pys.bulk_metrics(urls))
     elif method == 'links':
-        url = resource
-        print(pys.links(url))
+        url = option1
+        if not option2:
+            help()
+            sys.exit()
+        elif option2 == 'ptp':
+            print(pys.links(url, Pyscape.L_PTP))
+        elif option2 == 'ptd':
+            print(pys.links(url, Pyscape.L_PTD))
+        else:
+            help()
+            sys.exit()
     elif method == 'anchor-text':
-        url = resource
-        print(pys.anchor_text(url))
+        url = option1
+        if not option2:
+            help()
+            sys.exit()
+        elif option2 == 'ptp':
+            print(pys.anchor_text(url, Pyscape.A_PTP))
+        elif option2 == 'ptd':
+            print(pys.anchor_text(url, Pyscape.A_PTD))
+        else:
+            help()
+            sys.exit()
 
 if __name__ == '__main__':
     sys.exit(main())
