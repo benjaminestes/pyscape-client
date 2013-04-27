83f456ad599774fbf5839973231494e9cd680be5
Benjamin Estes <benjamin.estes@distilled.net>
2013-04-26 18:34:58 -0700

    initial build of API response code reference

--- util/buildflags.py
+++ util/buildflags.py
@@ -1,8 +1,33 @@
 #!/usr/bin/python3
 
+# Super hack-y method of converting CSV values into a JSON string
+# corresponding to all of the return values of the SEOmoz API.
+#
+# Works.
+
 import csv
-import json
 import sys
 
+HEADERS = ['response','human', 'flag', 'free', 'category']
+
+def main():
+    source = sys.argv[1] if len(sys.argv) > 1 else None
+
+    if not source:
+        exit('Invalid filename.')
+
+    data = {}
+
+    with open(source, 'r') as f:
+        for row in csv.reader(f):
+            row_data = {}
+            for i in range(1,5):
+                row_data[HEADERS[i]] = row[i]
+                
+            data[row[0]] = row_data
+
+    with open('bitflags.json', 'w') as w:
+        w.write(str(data))
+
 if __name__ == '__main__':
-    pass
+    sys.exit(main())
