9f60cbe5091ada164817d99d0ee695b5ca29e158
Benjamin Estes <benjamin.lee.estes@gmail.com>
2015-03-01 10:42:50 -0800

    Added endpoints test.

---
+++ tests/test_endpoints.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env/ python3
+
+import unittest
+
+from pyscape import Pyscape
+
+from config import VALID_KEYS
+
+class TestSequenceFunctions(unittest.TestCase):
+
+    def setUp(self):
+        p = Pyscape(**VALID_KEYS)
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
